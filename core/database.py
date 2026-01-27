e60b546738ed04ac2b743d68972f373b4cf8490c
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2026-01-27 10:27:02 -0300

    docs: docstring do database.py

--- core/database.py
+++ core/database.py
@@ -3,6 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from .config import settings
+"""
+Módulo de configuração do banco de dados utilizando SQLAlchemy.
+
+Este arquivo estabelece a conexão com o banco de dados, define o local do 
+banco de dados e a classe base para os modelos declarativos (Base) 
+da aplicação, utilizando as configurações carregadas do ambiente.
+"""
 
 engine = create_engine(
     settings.DATABASE_URL, connect_args={"check_same_thread": False}
