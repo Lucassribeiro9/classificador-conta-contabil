377d87f5d2cb7374ad478d86ca4804861091c454
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2026-02-09 19:37:41 -0300

    refactor: alterando Settings para ignorar outras variaveis no env

--- core/config.py
+++ core/config.py
@@ -1,10 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
+
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./classificador.db")
 
-    class Config:
-        env_file = ".env"
-
+    model_config = SettingsConfigDict(
+        env_file=".env", extra="ignore"  # Ignora variáveis de ambiente extras
+    )
 settings = Settings()
